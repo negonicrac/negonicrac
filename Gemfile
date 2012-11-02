7cd067e29cef25810d800845993369a0af103746
Louis T <louis@negonicrac.com>
2012-11-01 22:05:38 -0400

    start using righthand

--- Gemfile
+++ Gemfile
@@ -2,17 +2,16 @@ source :rubygems
 
 gem "rake"
 gem "slim"
-gem "redcarpet"
-gem "ruby-oembed"
 gem "builder"
 gem "uglifier"
+
+gem "righthand", path: "~/Development/ruby/righthand"
 
 gem "middleman", github: "middleman/middleman"
 gem "middleman-blog"
 gem "middleman-livereload"
 gem "middleman-smusher"
 gem "middleman-minify-html"
-# gem "middleman-sync"
 
 gem "bootstrap-sass"
 
--- config.rb
+++ config.rb
@@ -1,48 +1,11 @@
-require File.expand_path("../lib/lassitude_redcarpet_html", __FILE__)
+require "righthand/html"
+require "righthand/helpers"
+require "righthand/monkeypatches/blog_article"
 
-module ::Middleman
-  module Blog
-    module BlogArticle
-      def author
-        a = data.author || "negonicrac"
-        @app.data.authors[a]
-      end
-
-      def to_json
-        serializable_hash.to_json
-      end
-
-      def published
-        !!data.published
-      end
-
-      def comments
-        !!data.comments
-      end
-
-      def serializable_hash(options = nil)
-        {
-          id: url.gsub(/.html$/,""),
-          url: url.gsub(/.html$/,".json"),
-          slug: slug,
-          mime_type: mime_type,
-          title: title,
-          display_title: data["eng_title"] || title,
-          date: date,
-          published: data.published,
-          comments: data.comments,
-          tags: tags,
-          summary: summary,
-          body: body,
-          author: {
-            name: author.name,
-            url: author.url,
-            email: author.email
-          }
-        }
-      end
-    end
-  end
+helpers do
+  include Righthand::Helpers::AuthorHelpers
+  include Righthand::Helpers::DateHelpers
+  include Righthand::Helpers::MetatagHelpers
 end
 
 activate :livereload
@@ -81,7 +44,7 @@ set :css_dir, "stylesheets"
 set :js_dir, "javascripts"
 set :images_dir, "images"
 set :markdown_engine, :redcarpet
-set :markdown, renderer: LassitudeRedcarpetHTML
+set :markdown, renderer: Righthand::HTML
 
 configure :build do
   set :js_compressor, Uglifier.new(toplevel: false, unsafe: true, copyright: false)
@@ -92,17 +55,4 @@ configure :build do
   activate :asset_hash
   activate :minify_css
   activate :minify_javascript
-
-  #deploy_configs = YAML::load_file(".fog.yml")
-  #if deploy_configs
-  #  activate :sync do |sync|
-  #    sync.fog_provider          = deploy_configs[:provider] # Your storage provider
-  #    sync.fog_directory         = deploy_configs[:bucket_name] # Your bucket name
-  #    sync.fog_region            = deploy_configs[:bucket_region_name] # The region your storage bucket is in
-  #    sync.aws_access_key_id     = deploy_configs[:aws_access_key_id] # Your Amazon S3 access key
-  #    sync.aws_secret_access_key = deploy_configs[:aws_secret_access_key] # Your Amazon S3 access secret
-  #    # sync.after_build         = true # Run sync after build
-  #    sync.existing_remote_files = "keep" # What to do with your existing remote files? (keep or delete)
-  #  end
-  #end
 end
